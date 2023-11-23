61c292ab25381d9437cb89b59ec97bb6fb8e6506
anyakazachkova <anya.kazachkova98@gmail.com>
2023-11-24 00:27:48 +0300

    add parser for vacancies from Superjob

---
+++ datalib/parsers.py
@@ -0,0 +1,170 @@
+import sys
+import os
+import requests
+import logging
+from datetime import timedelta, datetime
+import pandas as pd
+import dask.dataframe as dd
+import numpy as np
+from bs4 import BeautifulSoup
+from typing import List, Dict, Any
+
+
+class SuperjobParser:
+    """
+    Данный класс осуществлеет выгрузку вакансий с сайта Superjob 
+    и сохраняет локально данные.
+    """
+
+    def __init__(self, 
+                 chunksize: int = 100000
+                 ) -> None:
+        
+        self.chunksize = chunksize
+        
+        self._get_logger()
+
+    def _get_logger(self):
+        self.logger = logging.Logger('logger')
+        handler = logging.StreamHandler(stream=sys.stdout)
+        self.logger.addHandler(handler)
+
+    def _parse_search_page(self, 
+                           params: Dict[str, Any] = dict()
+                           ) -> Dict[str, str]:
+
+        hrefs, names = [], []
+
+        page = 1
+        while True:
+            self.logger.info(f'Start parsing vacancies: {params}: page {page}')
+            response = requests.get(
+                'https://www.superjob.ru/vacancy/search/', 
+                params={
+                    **params,
+                    'page': page
+                }
+            )
+
+            if response.status_code in [200, 201, 202]:
+                tree = BeautifulSoup(response.content, 'html.parser')
+                items = tree.find_all(
+                    'span', 
+                    {
+                        'class': '_3j-5N _2BF76 _1Sjvs _21_a3 _2yOtM _1htjG _65Aie _3WSA8'
+                    }
+                )
+
+                if len(items) == 0:
+                    self.logger.info(f'No vacancies have been found on page {page}, stop searching')
+                    break
+                
+                self.logger.info(f'Success: {len(items)} vacancies have been found')
+
+                for item in items:
+                    hrefs.append(item.a.get('href'))
+                    names.append(item.a.text)
+                
+                page += 1
+            
+            else:
+                self.logger.info(f'Parsing error: {response.text}')
+
+        self.vacancies_list = zip(names, hrefs)
+
+        return self.vacancies_list
+
+    def _parse_vacancy_page(self,
+                            href: str, 
+                            name: str, 
+                            params: Dict[str, Any] = dict()
+                            ) -> Dict[str, Any]:
+        response = requests.get(
+            f'https://www.superjob.ru/{href}', 
+            params={
+                **params,
+            }
+        )
+
+        vacancy_info = {
+            'name': name,
+            'url': f'https://www.superjob.ru/{href}'
+        }
+
+        if response.status_code in [200, 201, 202]:
+            tree = BeautifulSoup(response.content, 'html.parser')
+
+            # get salary
+            salary = tree.find_all(
+                'span', 
+                {
+                    'class': '_2eYAG _2yOtM _1htjG _3GXPo'
+                }
+            )[0].text
+            vacancy_info['salary'] = ''.join(filter(str.isdigit, salary))
+
+            # get education requirement
+            vacancy_info['education'] = tree.find_all(
+                'span',
+                {
+                    'class': '_32IJo _1htjG _3hV-5'
+                }
+            )[0].text.capitalize()
+
+            # get address, experience, employment
+            vacancy_info['address'] = tree.find_all(
+                'span',
+                {
+                    'class': '_1qYY4'
+                }
+            )[0].text
+
+            # get other info
+            other_info = tree.find_all(
+                'span',
+                {
+                    'class': '_32IJo _1htjG _3GXPo'
+                }
+            )
+            vacancy_info['experience'], \
+                vacancy_info['employment'] =  \
+                        other_info[-2].text, other_info[-1].text.capitalize()
+            
+            # get description
+            vacancy_info['description'] = tree.find_all(
+                'span',
+                {
+                    'class': '_39I1Z Rg6Ej _1htjG _65Aie _3GXPo _3fUkL'
+                }
+            )[0].text
+
+            self.logger.info(f'Successfully parsed: {href}')
+        
+            return vacancy_info
+        
+        else:
+            self.logger.info(f'Error while parsing vacancy {href}: {response.text}')
+            raise ValueError(response.text)
+
+    def parse_vacancies(self, 
+                        params: Dict[str, Any] = dict()
+                        ):
+        vacancies_list = self._parse_search_page(params)
+
+        result = []
+        for key, value in vacancies_list:
+            try:
+                vacancy_info = self._parse_vacancy_page(value, key)
+                result.append(vacancy_info)
+            except Exception as e:
+                self.logger.info(f'Error while parsing vacancy {value}: {e}')
+        
+        return pd.DataFrame(result)
+    
+    def save_result(self, 
+                    df: pd.DataFrame, 
+                    path: str
+                    ) -> None:
+        os.makedirs(path, exist_ok=True)
+        dd.from_pandas(df, chunksize=self.chunksize) \
+            .to_parquet(path)
---
+++ main.py
@@ -0,0 +1,14 @@
+from datalib.parsers import SuperjobParser
+
+
+if __name__ == '__main__':
+
+    # get data
+    params = {'keywords': 'Python'}
+
+    parser = SuperjobParser()
+    df = parser.parse_vacancies(params)
+    parser.save_result(
+        df,
+        'parsed_data'
+    )
